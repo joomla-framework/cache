476e876c514f0022ebe0744d8f9355c1f7d6f03f
Michael Babker <mbabker@flbab.com>
2016-03-31 10:28:47 -0400

    Add native APCu handler

---
+++ Tests/ApcuTest.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Cache\Tests;
+
+use Joomla\Cache;
+
+/**
+ * Tests for the Joomla\Cache\Apcu class.
+ *
+ * @since  1.0
+ */
+class ApcuTest extends CacheTest
+{
+	/**
+	 * Tests for the correct Psr\Cache return values.
+	 *
+	 * @return  void
+	 *
+	 * @coversNothing
+	 * @since   1.0
+	 */
+	public function testPsrCache()
+	{
+		$this->assertInternalType('boolean', $this->instance->clear(), 'Checking clear.');
+		$this->assertInstanceOf('\Psr\Cache\CacheItemInterface', $this->instance->getItem('foo'), 'Checking getItem.');
+		$this->assertInternalType('array', $this->instance->getItems(array('foo')), 'Checking getItems.');
+		$this->assertInternalType('boolean', $this->instance->deleteItem('foo'), 'Checking deleteItem.');
+		$this->assertInternalType('array', $this->instance->deleteItems(array('foo')), 'Checking deleteItems.');
+
+		// Create a stub for the CacheItemInterface class.
+		$stub = $this->getMockBuilder('\\Psr\\Cache\\CacheItemInterface')
+			->getMock();
+
+		$stub->method('get')
+			->willReturn('bar');
+
+		$stub->method('getKey')
+			->willReturn('foo');
+
+		$this->assertInternalType('boolean', $this->instance->save($stub), 'Checking save.');
+	}
+
+	/**
+	 * Tests the Joomla\Cache\Apcu::clear method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Cache\Apcu::clear
+	 * @since   1.0
+	 */
+	public function testClear()
+	{
+		$this->assertTrue($this->instance->clear());
+	}
+
+	/**
+	 * Tests the Joomla\Cache\Apcu::hasItem method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Cache\Apcu::hasItem
+	 * @since   1.0
+	 */
+	public function testHasItem()
+	{
+		$this->assertTrue($this->instance->hasItem('foo'));
+	}
+
+	/**
+	 * Tests the Joomla\Cache\Apcu::get method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Cache\Apcu::get
+	 * @since   1.0
+	 */
+	public function testGet()
+	{
+		$this->assertInstanceOf(
+			'\Psr\Cache\CacheItemInterface',
+			$this->instance->getItem('foo')
+		);
+	}
+
+	/**
+	 * Tests the Joomla\Cache\Apcu::remove method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Cache\Apcu::remove
+	 * @since   1.0
+	 */
+	public function testRemove()
+	{
+		$this->assertTrue($this->instance->deleteItem('foo'));
+	}
+
+	/**
+	 * Tests the Joomla\Cache\Apcu::save method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Cache\Apcu::save
+	 * @since   1.0
+	 */
+	public function testSave()
+	{
+		// Create a stub for the CacheItemInterface class.
+		$stub = $this->getMockBuilder('\\Psr\\Cache\\CacheItemInterface')
+			->getMock();
+
+		$stub->method('get')
+			->willReturn('car');
+
+		$stub->method('getKey')
+			->willReturn('boo');
+
+		$this->assertTrue($this->instance->save($stub));
+	}
+
+	/**
+	 * Setup the tests.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function setUp()
+	{
+		$this->cacheClass = 'Joomla\\Cache\\Apcu';
+
+		try
+		{
+			parent::setUp();
+
+			// Create a stub for the CacheItemInterface class.
+			$stub = $this->getMockBuilder('\\Psr\\Cache\\CacheItemInterface')
+				->getMock();
+
+			$stub->method('get')
+				->willReturn('bar');
+
+			$stub->method('getKey')
+				->willReturn('foo');
+
+			$this->instance->save($stub);
+		}
+		catch (\Exception $e)
+		{
+			$this->markTestSkipped();
+		}
+	}
+}
---
+++ src/Apcu.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Part of the Joomla Framework Cache Package
+ *
+ * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Cache;
+
+use Joomla\Cache\Exception\UnsupportedFormatException;
+use Joomla\Cache\Item\HasExpirationDateInterface;
+use Psr\Cache\CacheItemInterface;
+use Joomla\Cache\Item\Item;
+
+/**
+ * APCu cache driver for the Joomla Framework.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class Apc extends Cache
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   mixed  $options  An options array, or an object that implements \ArrayAccess
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 * @throws  \RuntimeException
+	 */
+	public function __construct($options = array())
+	{
+		if (!extension_loaded('apc') || !is_callable('apcu_fetch'))
+		{
+			throw new UnsupportedFormatException('APC not supported.');
+		}
+
+		parent::__construct($options);
+	}
+
+	/**
+	 * This will wipe out the entire cache's keys
+	 *
+	 * @return  boolean  The result of the clear operation.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function clear()
+	{
+		return apcu_clear_cache('user');
+	}
+
+	/**
+	 * Method to get a storage entry value from a key.
+	 *
+	 * @param   string  $key  The storage entry identifier.
+	 *
+	 * @return  CacheItemInterface
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 * @throws  \RuntimeException
+	 */
+	public function getItem($key)
+	{
+		$success = false;
+		$value = apcu_fetch($key, $success);
+		$item = new Item($key);
+
+		if ($success)
+		{
+			$item->set($value);
+		}
+
+		return $item;
+	}
+
+	/**
+	 * Obtain multiple CacheItems by their unique keys.
+	 *
+	 * @param   array  $keys  A list of keys that can obtained in a single operation.
+	 *
+	 * @return  array  An associative array of CacheItem objects keyed on the cache key.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function getItems(array $keys = array())
+	{
+		$items = array();
+		$success = false;
+		$values = apcu_fetch($keys, $success);
+
+		if ($success && is_array($values))
+		{
+			foreach ($keys as $key)
+			{
+				$items[$key] = new Item($key);
+
+				if (isset($values[$key]))
+				{
+					$items[$key]->set($values[$key]);
+				}
+			}
+		}
+
+		return $items;
+	}
+
+	/**
+	 * Method to remove a storage entry for a key.
+	 *
+	 * @param   string  $key  The storage entry identifier.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function deleteItem($key)
+	{
+		if ($this->hasItem($key))
+		{
+			return apcu_delete($key);
+		}
+
+		// If the item doesn't exist, no error
+		return true;
+	}
+
+	/**
+	 * Persists a cache item immediately.
+	 *
+	 * @param   CacheItemInterface  $item  The cache item to save.
+	 *
+	 * @return static
+	 *   The invoked object.
+	 */
+	public function save(CacheItemInterface $item)
+	{
+		// If we are able to find out when the item expires - find out. Else bail.
+		if ($item instanceof HasExpirationDateInterface)
+		{
+			$ttl = $this->convertItemExpiryToSeconds($item);
+		}
+		else
+		{
+			$ttl = 0;
+		}
+
+		return apcu_store($item->getKey(), $item->get(), $ttl);
+	}
+
+	/**
+	 * Method to determine whether a storage entry has been set for a key.
+	 *
+	 * @param   string  $key  The storage entry identifier.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function hasItem($key)
+	{
+		return apcu_exists($key);
+	}
+}
