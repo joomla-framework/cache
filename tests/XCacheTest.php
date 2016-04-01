2d0120a62ff94a2c1814944087203e6b2903381f
wilsonge <georgejameswilson@googlemail.com>
2016-04-01 08:50:55 +0100

    Extend XCache tests from the generic CacheTest

--- tests/XCacheTest.php
+++ tests/XCacheTest.php
@@ -13,108 +13,8 @@ use Joomla\Cache;
  *
  * @since  1.0
  */
-class XCacheTest extends \PHPUnit_Framework_TestCase
+class XCacheTest extends CacheTest
 {
-	/**
-	 * @var    Cache\XCache
-	 * @since  1.0
-	 */
-	private $instance;
-
-	/**
-	 * Tests for the correct Psr\Cache return values.
-	 *
-	 * @return  void
-	 *
-	 * @coversNothing
-	 * @since   1.0
-	 */
-	public function testPsrCache()
-	{
-		$this->assertInternalType('boolean', $this->instance->clear(), 'Checking clear.');
-		$this->assertInstanceOf('\Psr\Cache\CacheItemInterface', $this->instance->getItem('foo'), 'Checking get.');
-		$this->assertInternalType('array', $this->instance->getItems(array('foo')), 'Checking getMultiple.');
-		$this->assertInternalType('boolean', $this->instance->deleteItem('foo'), 'Checking remove.');
-		$this->assertInternalType('boolean', $this->instance->deleteItems(array('foo')), 'Checking removeMultiple.');
-
-		// Create a stub for the CacheItemInterface class.
-		$stub = $this->getMockBuilder('\\Psr\\Cache\\CacheItemInterface')
-			->getMock();
-
-		$stub->method('get')
-			->willReturn('bar');
-
-		$stub->method('getKey')
-			->willReturn('foo');
-
-		$this->assertInternalType('boolean', $this->instance->save($stub), 'Checking set.');
-	}
-
-	/**
-	 * Tests the Joomla\Cache\XCache::clear method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Cache\XCache::clear
-	 * @since   1.0
-	 */
-	public function testClear()
-	{
-		$this->markTestIncomplete();
-	}
-
-	/**
-	 * Tests the Joomla\Cache\XCache::hasItem method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Cache\XCache::hasItem
-	 * @since   1.0
-	 */
-	public function testHasItem()
-	{
-		$this->markTestIncomplete();
-	}
-
-	/**
-	 * Tests the Joomla\Cache\XCache::getItem method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Cache\XCache::getItem
-	 * @since   1.0
-	 */
-	public function testGetItem()
-	{
-		$this->markTestIncomplete();
-	}
-
-	/**
-	 * Tests the Joomla\Cache\XCache::deleteItem method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Cache\XCache::deleteItem
-	 * @since   1.0
-	 */
-	public function testDeleteItem()
-	{
-		$this->markTestIncomplete();
-	}
-
-	/**
-	 * Tests the Joomla\Cache\XCache::set method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Cache\XCache::set
-	 * @since   1.0
-	 */
-	public function testSet()
-	{
-		$this->markTestIncomplete();
-	}
-
 	/**
 	 * Setup the tests.
 	 *
@@ -129,8 +29,7 @@ class XCacheTest extends \PHPUnit_Framework_TestCase
 			$this->markTestSkipped('XCache Cache Handler is not supported on this system.');
 		}
 
+		$this->cacheClass = 'Joomla\\Cache\\XCache';
 		parent::setUp();
-
-		$this->instance = new Cache\XCache;
 	}
 }
